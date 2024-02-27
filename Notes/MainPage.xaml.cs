85a95175399db6bf4bfca9868aa002fe7e47dc64
CHris <Christian.ROCampos@gmail.com>
2024-02-26 21:41:11 -0300

    chore:Added.gitignore

---
+++ Notes/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace Notes
+{
+
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+        string nome = "";
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+        private void OnNameClicked(object sender, EventArgs e)
+        {
+            nome = NameEntry.Text;
+            GreetingLabel.Text = $"Hello, {nome}";
+            SemanticScreenReader.Announce(GreetingLabel.Text);
+        }
+
+        private void HelloBtn_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
